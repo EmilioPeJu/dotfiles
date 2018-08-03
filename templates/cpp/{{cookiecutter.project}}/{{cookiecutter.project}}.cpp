d29c3799ea70451972c912e81bd3f5d5e8c465c5
hir12111 <hir12111@gmail.com>
2018-08-04 00:00:00 +0100

    Initial simple makefile cpp project template

---
+++ templates/cpp/{{cookiecutter.project}}/{{cookiecutter.project}}.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef tuple<int, int, int> iii;
+typedef vector<ii > vii;
+typedef vector<iii > viii;
+
+int main() {
+	ios::sync_with_stdio(false);
+
+	return 0;
+}
