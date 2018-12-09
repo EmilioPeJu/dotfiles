718ab429db4819cdfc2f5ab485270f0a24382d56
hir12111 <hir12111@gmail.com>
2018-12-09 21:26:23 +0000

    Changes for using gnome

--- .config/init.sh
+++ .config/init.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 # runlog
-xfce4-power-manager
-compton &
+#xfce4-power-manager
+#compton &
 #conky -d -b -c ${HOME}/.config/conky/conky.conf
 #conky -d -b -c ${HOME}/.config/conky/conky2.conf
 xmodmap ${HOME}/.Xmodmap
+trayer &
+albert &
+keybase-gui
+/soft/Telegram/Telegram
 #/code/scripts/xrand-2.sh
 #xset mouse 2 9
-dunst &
-nitrogen --restore
-#albert &
+#dunst &
+#nitrogen --restore
 tilda & # bash
 ( sleep 1 && tilda ) & # ranger
 ( sleep 2 && tilda ) & # python2
-volumeicon &
+#volumeicon &
 #autocutsel &
 #autocutsel -s PRIMARY &
 # basket -h &
