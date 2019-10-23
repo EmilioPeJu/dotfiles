1d676dbf3dea8eba5bc53ed7c8aa47bf5ffc673e
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2019-10-23 09:55:36 +0100

    Extract epics version from environment variable

--- .config/ranger/hir12111env.py
+++ .config/ranger/hir12111env.py
@@ -1,10 +1,8 @@
 import os
 import re
 
-# EPICS_BASE = os.environ["EPICS_BASE"]
-# EPICS_VERSION = EPICS_BASE.split('/')[3]
-# override for using the old one
-EPICS_VERSION = "R3.14.12.3"
+EPICS_BASE = os.environ["EPICS_BASE"]
+EPICS_VERSION = EPICS_BASE.split('/')[3]
 
 if os.access('/etc/redhat-release', os.R_OK):
     rhel_release_data = open("/etc/redhat-release", "r").read()
