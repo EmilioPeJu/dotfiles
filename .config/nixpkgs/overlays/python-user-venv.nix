7d389f7af0e3079192b40a587029996cd9cc6388
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2020-06-21 21:08:56 +0100

    nix: add an overlay for generating a custom user venv

---
+++ .config/nixpkgs/overlays/python-user-venv.nix
@@ -0,0 +1,14 @@
+self: super:
+
+{
+  python-user = super.python3.withPackages
+    (pp: with pp; [
+      pyzmq
+    ]);
+  python-user-venv = super.buildEnv {
+    name = "python-user-venv";
+    paths = [
+      self.python-user
+    ];
+  };
+}
