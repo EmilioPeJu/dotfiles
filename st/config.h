bce322a7b16797e501ad8d5dcbc194c66ac7f8de
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2020-06-22 14:22:46 +0100

    st: use zsh from PATH

--- st/config.h
+++ st/config.h
@@ -17,7 +17,7 @@ static int borderpx = 2;
  * 5: value of shell in config.h
  */
 static char *shell = "/bin/sh";
-char *utmp = "/run/current-system/sw/bin/zsh";
+char *utmp = "zsh";
 /* scroll program: to enable use a string like "scroll" */
 char *scroll = NULL;
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
