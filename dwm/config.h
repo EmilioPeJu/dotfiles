52e49ab64c43901bc33d1c593c747aa5fad4be45
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2020-06-22 15:29:43 +0100

    dwm: add a few shortcuts
    Taken from Luke

--- dwm/config.h
+++ dwm/config.h
@@ -58,7 +58,6 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *lockcmd[]  = { "i3lock", "-c", "000000", NULL };
-static const char *ranger[]  = { "st", "-e", "ranger", NULL };
 static const char *shutdowncmd[]  = { "shutdown", "now", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *volctl[]  = {"pavucontrol", NULL};
@@ -70,14 +69,16 @@ static const char *webcmd[]  = { "firefox", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd } },
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ 0,                            XK_Print,  spawn,          SHCMD("maim $(date +%y%m%d-%H%M-%S).png") },
 	{ShiftMask,                     XK_Print,  spawn,          SHCMD("maim -s $(date +%y%m%d-%H%M-%S).png") },
-	{ MODKEY,                       XK_r,      spawn,          {.v = ranger } },
+	{ MODKEY,                       XK_r,      spawn,          SHCMD("st -e ranger") },
+	{ MODKEY|ShiftMask,             XK_r,      spawn,          SHCMD("st -e htop") },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = shutdowncmd } },
 	{ MODKEY,                       XK_v,      spawn,          {.v = volctl } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = webcmd } },
+	{ MODKEY|ShiftMask,             XK_w,      spawn,          SHCMD("st -e nmtui") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
