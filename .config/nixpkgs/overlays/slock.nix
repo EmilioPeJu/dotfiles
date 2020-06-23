3d8fe8ac4e3f20c56e7ab09d719c4a9456e57e15
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2020-06-23 23:39:23 +0100

    Add overlay for slock

---
+++ .config/nixpkgs/overlays/slock.nix
@@ -0,0 +1,22 @@
+self: super:
+
+{
+  slock = super.stdenv.mkDerivation rec {
+    name="slock";
+    buildInputs=[
+      super.xorg.libX11
+      super.xorg.libXext
+      super.xorg.libXrandr
+      super.xorg.xorgproto
+    ];
+    configFile=~/suckless/slock/config.h;
+    src=~/suckless/slock/src;
+    postPatch=''
+      sed -i '/chmod u+s/d' Makefile
+      cp -f ${configFile} config.h
+    '';
+    installPhase = ''
+      make install PREFIX=$out
+    '';
+  };
+}
