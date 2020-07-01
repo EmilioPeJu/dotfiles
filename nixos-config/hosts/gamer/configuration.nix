b034e851d1f9968f6fa95f803d7934671aaaa20a
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2020-07-01 03:58:04 +0100

    Avoid editing boot line

--- nixos-config/hosts/gamer/configuration.nix
+++ nixos-config/hosts/gamer/configuration.nix
@@ -17,6 +17,7 @@
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.editor = false;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Kernel
