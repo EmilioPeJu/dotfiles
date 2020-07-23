5ea142289bcdba1446fc71e549268a94b757f3ee
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2020-07-23 20:38:16 +0100

    Add 9base recipes

--- suckless/install.sh
+++ suckless/install.sh
@@ -2,10 +2,12 @@
 name="$(basename $PWD)"
 cp -rf src build
 pushd build
-cp -rf ../config.h .
+[[ -f ../config.h ]] && cp -rf ../config.h .
 for patch in ../*.diff; do
     patch -p1 < $patch
 done
-make install PREFIX=$PREFIXPATH/$name &&
-popd &&
-rm -rf build
+if command -v musl-gcc &>/dev/null; then
+    make install PREFIX=$PREFIXPATH/$name CC=musl-gcc && popd && rm -rf build
+else
+    make install PREFIX=$PREFIXPATH/$name && popd && rm -rf build
+fi
