c322d056b5f7d2661d502fa79773698b8bed0544
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2020-07-24 10:05:48 +0100

    bashrc: tidy up a little bit

---
+++ .bash/variables.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# user environment variables
+#export SHELL="$(which zsh)"
+export PATH="$PATH:$HOME/scripts"
+export PATH="$PATH:$HOME/.local/bin"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/.local/lib"
+export PATH="$PATH:$HOME/.node/bin:$HOME/.cargo/bin"
+export PATH="$PATH:$HOME/.golang/bin:$HOME/.go/bin"
+export NODE_PATH="$NODE_PATH:$HOME/.node/lib/node_modules"
+export GOPATH="$HOME/.go"
+export EDITOR="vim"
+export TERMINAL="st"
+export PREFIXPATH="$HOME/prefix"
+export SRCPATH="$HOME/src"
+export NIX_PATH="$HOME/.nix-defexpr-user:$NIX_PATH"
+if command -v find-epics-base &> /dev/null; then
+    export EPICS_BASE=$(find-epics-base)
+fi
+export EPICS_CA_MAX_ARRAY_BYTES=15000000
+# Fix misbehaving java applications in dwm
+export _JAVA_AWT_WM_NONREPARENTING=1
+
