cd546d2a9ce3f9d4d9020b8c7175d9d95e5b0cf7
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2020-08-31 01:20:25 +0100

    Change to font OpenDyslexic Mono for main programs

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "OpenDyslexicMono:size=12";
 static int borderpx = 2;
 float alpha = 0.8;
 
