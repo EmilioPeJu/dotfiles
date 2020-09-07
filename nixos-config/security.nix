9a643eece8721eccc5cce5777e65bd058acbd341
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2020-09-08 00:46:15 +0100

    Update path to mount command

--- nixos-config/security.nix
+++ nixos-config/security.nix
@@ -4,8 +4,8 @@
   # Security
   security.sudo.enable = true;
   security.sudo.configFile = ''
-    user ALL=(ALL) NOPASSWD:/run/current-system/sw/bin/mount
-    user ALL=(ALL) NOPASSWD:/run/current-system/sw/bin/umount
+    user ALL=(ALL) NOPASSWD:/run/wrappers/bin/mount
+    user ALL=(ALL) NOPASSWD:/run/wrappers/bin/umount
     user ALL=(ALL) NOPASSWD:/run/current-system/sw/bin/nmtui-connect
     user ALL=(ALL) NOPASSWD:/run/current-system/sw/bin/systemctl
     user ALL=(ALL) NOPASSWD:/run/current-system/sw/bin/zfs
