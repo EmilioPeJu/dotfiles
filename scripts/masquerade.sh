8a3c3c6e1ee050ffeeae1436a52454dff9feadcd
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2020-11-22 23:38:40 +0000

    Tweak a few scripts

--- scripts/masquerade.sh
+++ scripts/masquerade.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # $1 input network interface
 # $2 output network interface
+iptables -P FORWARD ACCEPT
 sysctl -w net.ipv4.ip_forward=1
 iptables -A FORWARD -i "$1" -j ACCEPT
 iptables -t nat -A POSTROUTING -o "$2" -j MASQUERADE
