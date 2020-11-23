da9aadf0fd5ade76ae1da250c9a7c22c5368fecf
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2020-11-23 02:43:23 +0000

    Start spacenavd

--- .config/init-x.sh
+++ .config/init-x.sh
@@ -6,6 +6,8 @@ systemctl start --user pulseaudio
 xrandr --output HDMI1 --auto --left-of eDP1
 # my beautiful headphones
 echo connect EB:06:EF:1D:A2:F5 | bluetoothctl
+# Required daemon for handling 3D mouse
+spacenavd
 # player server
 one-instance mpd
 # notification daemon
@@ -18,3 +20,5 @@ $HOME/suckless/dwm/dwmbar &
 conky -d
 # keyboard layout
 setxkbmap us -variant altgr-intl -option ctrl:nocaps
+# set background
+~/.fehbg &
