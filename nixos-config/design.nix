110e32081f74ca310838bbe5ebbba4db05892d21
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2020-11-24 12:06:15 +0000

    Add openscad with 3D mouse support

--- nixos-config/design.nix
+++ nixos-config/design.nix
@@ -6,8 +6,17 @@ let
 in
 {
   environment.systemPackages = with pkgs; [
+    # freecad with 3D mouse support
     (freecad.overrideAttrs (oldAttrs: {
       buildInputs = oldAttrs.buildInputs ++ [libspnav];
+    }))
+    # openscad with 3D mouse support
+    (openscad.overrideAttrs (oldAttrs: {
+      buildInputs = oldAttrs.buildInputs ++ [ libspnav ];
+      postPatch = ''
+        sed -i '$ a SPNAV_INCLUDEPATH = '"${libspnav}/include" openscad.pro
+        sed -i '$ a SPNAV_LIBPATH = '"${libspnav}/lib" openscad.pro
+      '';
     }))
     spacenavd
   ];
