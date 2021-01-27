6dce4803c4f2ec451c27287e2576150288b70762
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2021-01-27 18:34:19 +0000

    Fix task-next-name script to only show pending tasks

--- scripts/task-next-name.py
+++ scripts/task-next-name.py
@@ -6,17 +6,18 @@ import subprocess
 
 def main():
     task_context = subprocess.check_output(["task", "context", "show"])
-    task_cmd = ["task"]
     context_match = re.match("Context '([^']*)' with filter '([^']+)'",
                              task_context.decode())
+    task_cmd = ["task"]
     if context_match:
         context_filter = context_match.group(2)
         task_cmd.append(context_filter)
     task_cmd.append("export")
     task_output = subprocess.check_output(task_cmd)
-    data = json.loads(task_output)
+    data = [item for item in json.loads(task_output) if item["status"] == "pending"]
     data.sort(key=lambda x: x["urgency"], reverse=True)
-    print(data[0]["description"])
+    if data:
+        print(data[0]['description'])
 
 
 if __name__ == "__main__":
