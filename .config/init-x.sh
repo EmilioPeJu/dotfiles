17206af3f808a8f82e1a846edc65c41b631c926b
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2021-02-17 19:33:40 +0000

    Start using clipmenud

--- .config/init-x.sh
+++ .config/init-x.sh
@@ -6,8 +6,6 @@ systemctl start --user pulseaudio
 xrandr --output HDMI1 --auto --left-of eDP1
 # my beautiful headphones
 echo connect EB:06:EF:1D:A2:F5 | bluetoothctl
-# Required daemon for handling 3D mouse
-spacenavd
 # player server
 one-instance mpd
 # notification daemon
@@ -20,5 +18,7 @@ $HOME/suckless/dwm/dwmbar &
 conky -d
 # keyboard layout
 setxkbmap us -variant altgr-intl -option ctrl:nocaps
+# clipboard manager
+clipmenud &
 # set background
 ~/.fehbg &
