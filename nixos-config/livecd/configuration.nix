d3891b60f6209773284d1cff5dbb82901c32d8c7
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2021-11-25 22:51:18 +0000

    Add configuration for creating a livecd

---
+++ nixos-config/livecd/configuration.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+let dirty = (import ../dirty.nix { });
+in {
+  imports = [
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+
+    # Provide an initial copy of the NixOS channel so that the user
+    # doesn't need to run "nix-channel --update" first.
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+    ./../base.nix
+    ./../desktop.nix
+  ];
+  networking.hostName = "machine";
+  networking.networkmanager.enable = false;
+  # users
+  users.users = {
+    user = {
+      isNormalUser = true;
+      uid = 1001;
+      extraGroups = [ "dialout" ];
+      hashedPassword = dirty.userHash;
+    };
+    root = { hashedPassword = dirty.rootHash; };
+  };
+}
+
