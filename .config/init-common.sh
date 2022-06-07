0ab20b487c832eed224f4258100dfe293f8af300
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2022-06-07 22:28:03 +0100

    Use gammastep for Xorg too

--- .config/init-common.sh
+++ .config/init-common.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # socket activation would not allow to connect immediately
 systemctl start --user pulseaudio
+
+gammastep &
 # player server
 one-instance mpd
 # my beautiful headphones
--- .config/init-wayland.sh
+++ .config/init-wayland.sh
@@ -6,7 +6,6 @@ chmod 600 "$clippath"
 ln -sf "$clippath" ~/.local/share/clipman.json
 # Run clipboard manager with no-persist to not loose rich content in copy
 wl-paste -p -t text --watch clipman store --no-persist &
-systemctl start --user gammastep
 # notification daemon
 mako &
 # lock when suspended
