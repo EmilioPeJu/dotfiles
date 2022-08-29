f788f2f476fee3cd999b5fbf440bbe358fb0d39c
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2022-08-29 15:49:37 +0100

    Add wrapper for passmenu which clears clipboard after 10 seconds

--- .config/init-wayland.sh
+++ .config/init-wayland.sh
@@ -6,7 +6,7 @@ chmod 600 "$clippath"
 ln -sf "$clippath" ~/.local/share/clipman.json
 # Run clipboard manager with no-persist to not lose rich content in copy
 wl-paste -t text --watch clipman store --no-persist &
-wl-paste -p -t text --watch clipman store --no-persist -P &
+wl-paste -p -t text --watch clipman store --no-persist &
 # notification daemon
 mako &
 # lock when suspended
