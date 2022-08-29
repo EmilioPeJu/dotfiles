f6703d85127a1b29caa726cf2a64dcd3b428ee8c
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2022-08-29 21:36:15 +0100

    Fix problem with outdated GPG_TTY

--- .bash/variables.sh
+++ .bash/variables.sh
@@ -7,7 +7,7 @@ export PATH="$PATH:$HOME/.node/bin:$HOME/.cargo/bin"
 export PATH="$PATH:$HOME/.golang/bin:$HOME/.go/bin"
 export NODE_PATH="$NODE_PATH:$HOME/.node/lib/node_modules"
 export GOPATH="$HOME/.go"
-export TERMINAL="st"
+export TERMINAL="alacritty"
 export PREFIXPATH="$HOME/prefix"
 export SRCPATH="$HOME/src"
 export PATH="$PATH:$SRCPATH/tools"
@@ -16,6 +16,8 @@ if type -p caget &> /dev/null; then
     export EPICS_BASE=$(readlink -f $(which caget))/../../
     export EPICS_CA_MAX_ARRAY_BYTES=15000000
 fi
+
+export GPG_TTY=$TTY
 
 # Fix misbehaving java applications in dwm
 export _JAVA_AWT_WM_NONREPARENTING=1
