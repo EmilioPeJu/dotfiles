0f77d3efc9e091d3cd0a35033726d7fc56f471d1
Emilio Perez <emiliopeju@gmail.com>
2022-11-09 00:24:59 +0000

    Simplify notification system
    Using ntfy instead of telegram bot

--- nixos-config/notify.nix
+++ nixos-config/notify.nix
@@ -1,44 +1,23 @@
 { config, pkgs, ... }:
 
-let
-  aiogram = (pkgs.python3Packages.callPackage ./pkgs/aiogram { });
-  hirnoty =
-    (pkgs.python3Packages.callPackage ./pkgs/hirnoty { aiogram = aiogram; });
-  dirty = (import ./dirty.nix { });
+let dirty = (import ./dirty.nix { });
 in {
-  environment.etc."hirnoty/config.py".source = pkgs.writeText "config" ''
-    TOKEN = "${dirty.botToken}";
-    ACL = [ ${dirty.userID} ]
-    SCRIPT_DIR = "/home/user/.config/hirnoty/scripts"
-    INDEX_DIR = "/home/user/.config/hirnoty/index"
-  '';
-  systemd.timers.sched-timer = {
+  systemd.timers.sched-timer1 = {
     wantedBy = [ "timers.target" ];
     partOf = [ "sched-timer.service" ];
     timerConfig.OnCalendar = "*-*-* 08:30:00";
   };
-  systemd.services.sched-timer = {
+  systemd.services.sched-timer1 = {
     serviceConfig.Type = "oneshot";
     serviceConfig.User = "user";
     script = ''
       export PATH=/run/current-system/sw/bin
       export USER=user
       export HOME=/home/user
-      remind /home/user/remind/top.rem '*1' | hirnoty-send -t sched
+      rems="$(remind /home/user/remind/top.rem '*1')"
+      ${pkgs.curl}/bin/curl -H "Priority: urgent" -d "$rems" ${dirty.ntfyUrl}
+      selected=$(sort -R /home/user/remind/lists/study.md | sed '/^#/d' | head -n1)
+      ${pkgs.curl}/bin/curl -H "Priority: urgent" -d "$selected" ${dirty.ntfyUrl}
     '';
   };
-  systemd.services."hirnoty" = {
-    enable = true;
-    unitConfig = {
-      Requires = "network-online.target";
-      After = "network-online.target";
-    };
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${hirnoty}/bin/hirnoty-ctl start";
-      User = "user";
-    };
-    wantedBy = [ "multi-user.target" ];
-  };
-  environment.systemPackages = with pkgs; [ hirnoty ];
 }
