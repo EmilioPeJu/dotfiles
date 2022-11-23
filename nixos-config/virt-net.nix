9b9a4b1acecd1eed8341aac18f71759313853512
Emilio Perez <emiliopeju@gmail.com>
2022-11-23 23:43:27 +0000

    virt: remove a few unused packages and move some parts to a separate file

---
+++ nixos-config/virt-net.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, ... }:
+
+{
+  networking.interfaces.tap0 = {
+    virtual = true;
+    virtualOwner = "user";
+    ipv4.addresses = [{
+      address = "172.16.0.1";
+      prefixLength = 16;
+    }];
+  };
+
+  # NFS server to let VMs use /nix software
+  services.nfs.server = {
+    enable = true;
+    exports = ''
+      /nix 172.16.0.0/16(rw,sync,no_subtree_check)
+      /home/user/src 172.16.0.0/16(rw,sync,no_subtree_check)
+    '';
+    lockdPort = 4001;
+  };
+
+  networking = {
+    firewall.allowedTCPPorts = [ 111 2049 4001 4045 20048 ];
+    firewall.allowedUDPPorts = [ 111 2049 4001 4045 20048 ];
+  };
+}
