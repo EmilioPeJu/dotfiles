b58bdfe098cd84624aab6162555a30d9f07a0389
Emilio Perez <emiliopeju@gmail.com>
2023-09-30 23:38:59 +0100

    Add me to group wheel

--- nixos-config/security-options.nix
+++ nixos-config/security-options.nix
@@ -3,24 +3,7 @@
 {
   # Security
   security.sudo.enable = true;
-  security.sudo.configFile = ''
-    user ALL=(ALL) NOPASSWD:/run/current-system/sw/bin/systemctl suspend
-    user ALL=(ALL) NOPASSWD:/run/wrappers/bin/mount -t nfs -o rw\,soft server1\:/rpool/user /ext/server
-    user ALL=(ALL) NOPASSWD:/run/wrappers/bin/umount /ext/server
-    user ALL=(ALL) NOPASSWD:/run/wrappers/bin/mount -t nfs -o rw\,soft arm1\:/mnt /ext/media
-    user ALL=(ALL) NOPASSWD:/run/wrappers/bin/umount /ext/media
-  '';
-  security.audit = {
-    backlogLimit = 8192;
-    enable = true;
-    failureMode = "printk";
-  };
-  #security.auditd.enable = true;
-  security.polkit.extraConfig = ''
-    polkit.addRule(function(action, subject) {
-      polkit.log("user " +  subject.user + " is attempting action " + action.id + " from PID " + subject.pid);
-    });
-  '';
+  users.users.user.extraGroups = [ "wheel" ];
   networking.firewall = {
     allowedTCPPorts = [ 1234 5064 6064 5065 6065 5075 6075 7011 7012 ];
     allowedUDPPorts = [ 5064 6064 5065 6065 5076 ];
