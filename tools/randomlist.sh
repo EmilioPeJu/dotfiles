cfaee6670c9dac041946522f69c55e26919f5c7d
Emilio Perez <emiliopeju@gmail.com>
2023-11-26 21:58:30 +0000

    Add a few tiny scripts

--- tools/randomlist.sh
+++ tools/randomlist.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-list_selected=$(ls ~/remind/lists/ | dmenu)
+if [ -n "$1" ]; then
+    list_selected="$1"
+else
+    list_selected=$(ls ~/remind/lists/ | dmenu)
+fi
+
 option_selected=$(sort -R ~/remind/lists/${list_selected} | sed '/^#/d' | head -n1)
 notify-send -- "${option_selected}"
