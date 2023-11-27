0abc1d69d547a6015a3dd225dbdd5a6450401b1d
Emilio Perez <emiliopeju@gmail.com>
2023-11-27 22:14:18 +0000

    Add kotlin in android related part

--- nixos-config/android.nix
+++ nixos-config/android.nix
@@ -4,9 +4,9 @@
   programs.adb.enable = true;
   environment.systemPackages = with pkgs; [
     abootimg
-    android-studio
+    #android-studio
     apktool
-    jdk11
+    kotlin
   ];
   services.udev.packages = [ pkgs.android-udev-rules ];
   users.users.user.extraGroups = [ "adbusers" ];
