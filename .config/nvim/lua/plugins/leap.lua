576c67665f514f79eddfb419c4719b175ff2442c
Emilio Perez <emiliopeju@gmail.com>
2024-04-18 10:53:44 +0100

    nvim: add plugin leap for a easier movement

---
+++ .config/nvim/lua/plugins/leap.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        'ggandor/leap.nvim',
+        config = function(_, opts)
+            local leap = require('leap')
+            leap.create_default_mappings()
+        end
+    }
+}
