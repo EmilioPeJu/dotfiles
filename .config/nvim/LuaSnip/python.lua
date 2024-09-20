126df1c1dd01096951a1c4d99fb8719ba60edd9f
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2024-09-20 14:36:24 +0100

    Add small snippet for a cocotb test template

--- .config/nvim/LuaSnip/python.lua
+++ .config/nvim/LuaSnip/python.lua
@@ -19,5 +19,32 @@ return {
                 parser.add_argument({})
                 return parser.parse_args()
         ]], {i(0)})
-    )
+    ),
+    s(
+        { trig = "cocotb" },
+        fmt([[
+            import cocotb
+
+            from cocotb.clock import Clock
+            from cocotb.triggers import RisingEdge, ClockCycles, ReadOnly
+            from cocotb_tools.runner import get_runner
+
+
+            @cocotb.test()
+            def example_test(dut):
+                cocotb.start_soon(Clock(dut.clk_i, 1, 'ns').start(start_high=False))
+                {}
+
+
+            def test_{}():
+                runner = get_runner('ghdl')
+                runner.build(sources=[ '{}.vhd' ],
+                             hdl_toplevel='{}',
+                             build_args=['--std=08'],
+                             always=True)
+                runner.test(hdl_toplevel='{}', test_args=['--std=08'],
+                            plusargs=['--vcd={}.vcd'],
+                            test_module='test_{}')
+        ]], {i(0), i(1), rep(1), rep(1), rep(1), rep(1), rep(1)})
+    ),
 }
