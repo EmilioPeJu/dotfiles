5a3b0d0f03cb8ceedc8192b7e68152e798ea428c
Emilio Perez <emiliopeju@gmail.com>
2025-07-18 00:02:10 +0100

    Add some aliases

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # user alias
+alias br='broot'
 alias cat2="cat << EOF >>"
 alias ju="cd ~/notes && jupyter lab"
 alias ga="git add"
@@ -19,8 +20,8 @@ alias gl="git log --color=always --graph --branches --pretty=format:'%Cred%h%Cre
 alias gp="git remote | xargs -L1 git push --all"
 alias gr="git rebase"
 alias java-bytecodes="javap -c -p -verbose -s";
+alias lg="lazygit"
 alias ls='ls --color=auto'
-alias n='nnn -e'
 alias remake="make clean uninstall; make"
 alias rename-latin1-to-utf8="convmv -f iso-8859-15 -t utf-8 --notest -r ."
 alias r2help="r2 -q -c '?*~...' -"
@@ -32,13 +33,19 @@ alias secd='search_cd'
 alias show-device-tree="dtc -I fs -O dts /proc/device-tree"
 # useful for simulating a serial port
 alias socat-pty="socat -d -d pty,rawer pty,rawer"
-alias t="trans -speak -d en:es"
 alias tmp="pushd /run/user/$(id -u)"
-alias r="ranger_cd"
-alias p="python3"
-alias ze="zellij"
-alias zl="zellij --layout"
 alias todrive='rclone sync "/home/user/drive" "google-drive:"'
 alias fromdrive='rclone sync "google-drive:" "/home/user/drive"'
-alias lg="lazygit"
+
+# One letter aliases
+alias b='sudo bpftrace'
+alias j='journalctl'
+alias k='kubectl'
+alias m='make'
+alias n='nnn -e'
+alias p="python3"
+alias r="ranger_cd"
+alias s='systemctl'
+alias t="trans -speak -d en:es"
+alias u='udevadm'
 alias v="$EDITOR"
