4814dbda962cb02bb4df884880c28b4d3163373a
Emilio Perez <emiliopeju@gmail.com>
2025-07-19 00:27:49 +0100

    Improve hypr config

--- .config/hypr/scripts/background-programs.sh
+++ .config/hypr/scripts/background-programs.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-hyprctl dispatch exec [ workspace special:firefox silent ] firefox
-hyprctl dispatch exec [ workspace special:magic silent ] kitty -e tmux
+hyprctl dispatch exec [ workspace special:firefox silent ] -- firefox
+hyprctl dispatch exec [ workspace special:magic silent ] -- kitty -e tmux
+hyprctl dispatch exec [ workspace 4 silent ] -- kitty -e journalctl -xe -f
+hyprctl dispatch exec [ workspace 4 silent ] -- kitty -e atop
+hyprctl dispatch exec [ workspace 5 silent ] -- Telegram
